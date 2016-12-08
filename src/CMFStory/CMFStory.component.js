e3c8d765075f2c794d50724ba7bcdff08c141e5f
Jean-Michel FRANCOIS <jmfrancois@talend.com>
2016-12-08 10:26:24 +0100

    fix: update API call to cmf

--- src/CMFStory/CMFStory.component.js
+++ src/CMFStory/CMFStory.component.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import { Provider } from 'react-redux';
-import { initializeStore, RegistryProvider } from 'react-cmf';
+import { store, RegistryProvider } from 'react-cmf';
 import mock from 'react-cmf/lib/mock';
 
 /**
@@ -18,11 +18,13 @@ class CMFStory extends React.Component {
 		if (!state) {
 			state = mock.state();
 		}
-		this.store = initializeStore(undefined, state);
+		this.store = store.initialize(undefined, state);
 	}
+
 	getChildContext() {
 		return { router: {} };
 	}
+
 	render() {
 		return (
 			<Provider store={this.store}>
@@ -35,13 +37,14 @@ class CMFStory extends React.Component {
 }
 
 CMFStory.propTypes = {
-	state: React.PropTypes.object,
+	state: PropTypes.object,
+	children: PropTypes.node,
 };
 CMFStory.contextTypes = {
-	registry: React.PropTypes.object,
+	registry: PropTypes.object,
 };
 CMFStory.childContextTypes = {
-	router: React.PropTypes.object,
+	router: PropTypes.object,
 };
 
 export default CMFStory;
